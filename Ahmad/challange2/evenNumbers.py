5da498c6f664a6ca2591b2e372222dd18138c04b
Muhammad Ahmad <33274476+ahmad-zigron@users.noreply.github.com>
2017-11-17 14:38:36 +0500

    Update evenNumbers.py

--- Ahmad/challange2/evenNumbers.py
+++ Ahmad/challange2/evenNumbers.py
@@ -1,3 +1,6 @@
+# Q: Write a program, which will find all such numbers between 1000 and 3000 (both included) such that each digit of the number is an even number.
+#The numbers obtained should be printed in a comma-separated sequence on a single line.
+
 print("Enter Range to find Even numbers between")
 while True:
  try:		
